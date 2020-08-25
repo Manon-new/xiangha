0942c0388af5c31f8e2829e6caf923d761382247
Manon-new <1574725174@qq.com>
2020-08-25 16:58:35 +0800

    WX825改

--- js/Restraint.js
+++ js/Restraint.js
@@ -1,11 +1,142 @@
+var food = {"foodlist":["./img/restraint/apple.jpg",
+			"./img/restraint/didiwei.jpg",
+			"./img/restraint/digua.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/yangtao.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/yangtao.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/ganlan.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/ganlan.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/ganlan.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/yangtao.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg",
+			"./img/restraint/ganzhe.jpg",
+			"./img/restraint/guiyuan.jpg",
+			"./img/restraint/apple.jpg"]
+		}
+var container = $(".container"),
+	body = $("body");
+	
 $(function(){
-	var i=0;
+	var i=0,arr=["#ff0","#f0f","#0ff","#00f","#0f0","#f00","#e5d","#a33","#33a","#3a3","sliver"],
+		j=Math.floor(Math.random());
+// 随机排序插入图片
+	var foodlist = food.foodlist
+	foodlist.sort(function(a,b){
+		return 0.5 - Math.random()
+	});
+	console.log(foodlist)
+	for(var i in foodlist){
+		// for(var m in $(".col-xs-1").children())
+		$($(".col-xs-1").children()[i]).removeAttr("src", foodlist[i])
+		$($(".col-xs-1").children()[i]).attr("src", foodlist[i]) 
+		console.log($(".col-xs-1").children())
+	}
+	
 	$(".col-xs-1").on("mouseenter","img",function(){
+		j=Math.floor(Math.random()*11)
 		$(this).css("display","none")
-		$(this).next().css("display","inline")
+		$(this).next().css({
+			"display":"block",
+			"width":"79px",
+			"height":"79px",
+			"font-size":"20px",
+			"line-height":"79px",
+			"text-align":"center",
+			"background-color":arr[j]
+		})
 	})
-	$("span").on("mouseout",function(){
-		console.log(++i)
-		
+	$(".col-xs-1 span").on("mouseout",function(){
+		// console.log(++i)
+		$(this).css("display","none")
+		$(this).prev().css({"display":"block"})
 	})
+	// 模态框
+	
 })
+
+
+		// <div class="row">
+		// 	<div class="col-xs-1"><img src="./img/restraint/apple.jpg" alt=""><span>苹果</span></div>
+		// 	<div class="col-xs-1"><img src="./img/restraint/apple.jpg" alt=""><span>苹果</span></div>
+		// 	<div class="col-xs-1"><img src="./img/restraint/apple.jpg" alt=""><span>苹果</span></div>
+		// 	<div class="col-xs-1"><img src="./img/restraint/apple.jpg" alt=""><span>苹果</span></div>
+		// 	<div class="col-xs-1"><img src="./img/restraint/apple.jpg" alt=""><span>苹果</span></div>
+		// 	<div class="col-xs-1"><img src="./img/restraint/apple.jpg" alt=""><span>苹果</span></div>
+		// 	<div class="col-xs-1"><img src="./img/restraint/apple.jpg" alt=""><span>苹果</span></div>
+		// 	<div class="col-xs-1"><img src="./img/restraint/apple.jpg" alt=""><span>苹果</span></div>
+		// 	<div class="col-xs-1"><img src="./img/restraint/apple.jpg" alt=""><span>苹果</span></div>
+		// 	<div class="col-xs-1"><img src="./img/restraint/apple.jpg" alt=""><span>苹果</span></div>
+		// 	<div class="col-xs-1"><img src="./img/restraint/apple.jpg" alt=""><span>苹果</span></div>
+		// 	<div class="col-xs-1"><img src="./img/restraint/apple.jpg" alt=""><span>苹果</span></div>
+		// </div>
